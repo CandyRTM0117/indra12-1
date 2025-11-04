6fcb610b748d0991afe2323a1e2fbb689dcf36b9
Luvsandamba <luvsandamba@Luvsandambas-MacBook-Air.local>
2025-11-04 11:04:38 +0800

    header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useState , useRef } from 'react';
 import './App.css';
+import icon from './img/schoolicon.png';
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const navbar = useRef(null)
+
+  const handleClick = () => {
+    setMenuOpen(!menuOpen);
+    if ( menuOpen === false ) {
+      navbar.current.style.right = '0'
+    }else {
+      navbar.current.style.right = '-100px'
+    }
+    console.log(menuOpen)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div>
+          <img src={icon} className="image" alt="iconschool" />
+        </div>
+
+        <button id="hams" onClick={handleClick}>
+          <div className={`hoverham ${menuOpen ? 'active' : ''}`}>
+            <div className="hambar hamfirst"></div>
+            <div className="hambar hamsecond"></div>
+            <div className="hambar hamthird"></div>
+          </div>
+        </button>
+
+        <div className={`hamburger ${menuOpen ? 'show' : ''}`} ref={navbar}>
+          <div>Teacher</div>
+          <div>News</div>
+          <div>About School</div>
+          <div>Contact Us</div>
+        </div>
+      </div>
     </div>
   );
 }
